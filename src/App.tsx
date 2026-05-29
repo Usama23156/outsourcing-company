4207ed817b7bee73cec624e941323432154eb89a
Usama23156 <usamaelkady25@gmail.com>
2026-05-29 20:08:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Hero from "@/components/components app/hero"
+
+function App() {
+  return (
+    <>
+{/* <div aria-hidden="true" className="pointer-events-none absolute inset-0 z-0">
+        <div className="absolute -left-28 -top-36 h-[360px] w-[360px] rounded-full blur-3xl opacity-25 bg-gradient-to-tr from-white via-indigo-500 to-indigo-400" />
+        <div className="absolute -bottom-44 -right-44 h-[420px] w-[420px] rounded-full blur-3xl opacity-20 bg-gradient-to-tr from-white via-emerald-400 to-emerald-500" />
+        <div className="absolute -inset-10 rotate-[-2deg] opacity-25 bg-[linear-gradient(to_right,rgba(255,255,255,0.09)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.08)_1px,transparent_1px)] bg-[size:44px_44px]" />
+      </div> */}
+    <Hero/>
+
+   
+    
+    </>
+  );
+}
+
+export default App;
